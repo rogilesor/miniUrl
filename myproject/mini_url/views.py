611af0f165d0c7292706fa9b70f8b568bb31568a
rogilesor <istarosel@yahoo.fr>
2015-10-29 21:32:10 +0100

    version finale'

--- myproject/mini_url/views.py
+++ myproject/mini_url/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404, render
+from mini_url.models import MiniUrl
+from mini_url.forms import MiniURLForm
 
-# Create your views here.
+
+def liste(request):
+    """ Affichage des redirections """
+    minis = MiniUrl.objects.order_by('-nb_acces')
+
+    return render(request, 'mini_url/liste.html', locals())
+
+
+def nouveau(request):
+    """ Ajout d'une redirection """
+    if request.method == "POST":
+        form = MiniURLForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(liste)
+    else:
+        form = MiniURLForm()
+
+    return render(request, 'mini_url/nouveau.html', {'form': form})
+
+
+def redirection(request, code):
+    """ Redirection vers l'URL enregistrée """
+    mini = get_object_or_404(MiniUrl, code=code)
+    mini.nb_acces += 1
+    mini.save()
+
+    return redirect(mini.url, permanent=True)
+
+
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -18,4 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^m/',include('mini_url.urls')),
 ]
